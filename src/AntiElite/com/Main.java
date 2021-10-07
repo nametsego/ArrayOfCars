6da2a38c80f6568b4e879224bddd84e5e0c43677
Nametsego <Nametsegothakadu@gmail.com>
2021-10-07 02:09:41 +0200

    3 set of cars in 2d array

---
+++ src/AntiElite/com/Main.java
@@ -0,0 +1,22 @@
+package AntiElite.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String cars[][] = {
+                {"Ford", "Porsche", "Corolla", "Jeep", "Ranger"},
+                {"Chevrolette", "Audi", "Lexus", "BMW", "Ferrari"},
+                {"Mahindra", "Toyota", "Isuzu", "Kia", "Haval"}
+        };
+
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println();
+            for (int j = 0; j < cars[i].length; j++) {
+                System.out.print(cars[i][j] + "  ");
+            }
+        }
+    }
+}
+
+
